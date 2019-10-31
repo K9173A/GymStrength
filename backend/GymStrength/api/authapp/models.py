bd901cc272d827850c4631a214f7eb99e309df06
K9173A <K9173A@gmail.com>
2019-10-31 20:57:32 +0000

    Add models for authapp and mainapp

--- backend/GymStrength/api/authapp/models.py
+++ backend/GymStrength/api/authapp/models.py
@@ -1,3 +1,57 @@
+"""
+Module for authapp models definitions.
+"""
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.dispatch import receiver
+from django.conf import settings
 
-# Create your models here.
+from djoser.signals import user_activated
+
+
+class User(AbstractUser):
+    """
+    Django user model implementation.
+    """
+    pass
+
+
+class UserProfile(models.Model):
+    """
+    UserProfile stores additional user information.
+    """
+    GENDER_CHOICES = (
+        (0, 'Not Set'),
+        (1, 'Male'),
+        (2, 'Female')
+    )
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    avatar = models.ImageField(
+        verbose_name='avatar',
+        upload_to=settings.USER_AVATARS_DIR,
+        blank=True
+    )
+    gender = models.IntegerField(
+        verbose_name='gender',
+        choices=GENDER_CHOICES,
+        default=0
+    )
+
+
+@receiver(user_activated)
+def save_user_profile(sender, user, request, **kwargs):
+    """
+    Creates a profile associated with activated User instance.
+    Triggered when user_activated signal is being emitted by the ActivationView.
+    :param sender: ActivationView subclass used to activate the user.
+    :param user: User instance representing the activated account.
+    :param request: HttpRequest in which the account was activated.
+    :param kwargs: additional key-value arguments.
+    :return:
+    """
+    (user_profile, created) = UserProfile.objects.get_or_create(user=user)
+    if created:
+        user_profile.save()
--- backend/GymStrength/api/mainapp/models.py
+++ backend/GymStrength/api/mainapp/models.py
@@ -1,3 +1,91 @@
+"""
+Module for mainapp models definitions.
+"""
 from django.db import models
 
-# Create your models here.
+
+class Muscle(models.Model):
+    """
+    Muscle model stores predefined set of muscle names which can be trained
+    in the training routine.
+    """
+    english_name = models.CharField(
+        max_length=128
+    )
+    latin_name = models.CharField(
+        max_length=128
+    )
+
+
+class Exercise(models.Model):
+    """
+    Exercise model stores information about all exercises used in the training
+    process.
+    """
+    name = models.CharField(
+        max_length=128
+    )
+    description = models.TextField(
+        max_length=256,
+        blank=True
+    )
+    image = models.ImageField(
+        blank=True
+    )
+    muscle = models.ForeignKey(
+        Muscle,
+        related_name='muscle_set',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+
+class Unit(models.Model):
+    """
+    Unit represents measuring system (kg, lbs and etc...).
+    """
+    short_name = models.CharField(
+        verbose_name='units',
+        max_length=16
+    )
+    full_name = models.CharField(
+        verbose_name='units',
+        max_length=64
+    )
+
+
+class Set(models.Model):
+    """
+    Set defines number of repetitions with required weight.
+    """
+    weight = models.FloatField(
+        default=1.0
+    )
+    repetition = models.IntegerField(
+        default=1
+    )
+    exercise = models.ForeignKey(
+        Exercise,
+        related_name='exercise_set',
+        on_delete=models.CASCADE
+    )
+    unit = models.ForeignKey(
+        Unit,
+        related_name='unit_set',
+        on_delete=models.CASCADE
+    )
+
+
+class Workout(models.Model):
+    """
+    Workout is a complex of exercises with set amount of sets and reps for
+    each exercise.
+    """
+    date = models.DateField()
+    name = models.CharField(
+        max_length=128,
+        blank=True
+    )
+    sets = models.ManyToManyField(
+        Set
+    )
