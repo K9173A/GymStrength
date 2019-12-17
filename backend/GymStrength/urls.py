37a58267dc35ee76ea25308f5d059233a3d59f64
K9173A <K9173A@gmail.com>
2019-12-17 17:38:57 +0000

    Apply settings needed for django admin to work

--- backend/GymStrength/urls.py
+++ backend/GymStrength/urls.py
@@ -4,11 +4,11 @@ Root GymStrength URL dispatcher. Handles all REST API requests.
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
 
 urlpatterns = [
     path('api/gym/', include('api.gymapp.urls', namespace='gym')),
-    # path('api/admin/', include('api.adminapp.urls', namespace='admin')),
+    path('api/admin/', admin.site.urls),
     # path('api/auth/', include('api.authapp.urls', namespace='auth')),
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
