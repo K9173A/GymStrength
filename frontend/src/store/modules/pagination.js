9efae20444c32ff9f3a2106f65a7791df9a278fe
K9173A <K9173A@gmail.com>
2019-12-18 18:32:15 +0000

    Optimized pagination module

--- frontend/src/store/modules/pagination.js
+++ frontend/src/store/modules/pagination.js
@@ -6,21 +6,36 @@ const state = {
     total: 1,
     count: 0,
   },
+  databaseExercise: {
+    prev: null,
+    next: null,
+    curr: 1,
+    total: 1,
+    count: 0,
+  },
+  workoutExercise: {
+    prev: null,
+    next: null,
+    curr: 1,
+    total: 1,
+    count: 0,
+  },
 };
 
-const actions = {};
-
 const mutations = {
+  /**
+   * Sets new value of the specified pagination element.
+   * @param state - Vuex object which stores states.
+   * @param paginationName - name of pagination object.
+   * @param elementName - name of pagination element (key).
+   * @param value - new value of pagination element (value).
+   */
   setPaginationElement(state, { paginationName, elementName, value }) {
     state[paginationName][elementName] = value;
   },
 };
 
-const getters = {};
-
 export default {
-  actions,
   mutations,
   state,
-  getters,
 };
