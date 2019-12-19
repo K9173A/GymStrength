8e6143c36dccba8f21547bb26ad1fe2299ae425b
K9173A <K9173A@gmail.com>
2019-12-19 13:42:28 +0000

    Add user creation with pass as argument v1

--- backend/create_db.sql
+++ backend/create_db.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE gymstrength;
+CREATE USER admin with PASSWORD ':v1';
 GRANT ALL PRIVILEGES ON DATABASE gymstrength TO admin;
 ALTER ROLE admin SET CLIENT_ENCODING TO 'UTF8';
 ALTER ROLE admin SET default_transaction_isolation TO 'READ COMMITTED';
